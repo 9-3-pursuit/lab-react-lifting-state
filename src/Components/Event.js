67488ea4d0f5e4e8048da618cc368569e5860dcf
joshuanelsondev <joshuanelsondev@gmail.com>
2023-02-09 00:01:10 -0500

    Complete lab

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,33 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees";
+
+export default function Event({events, updateEventAttendance}) {
+
+ 
+  return(
+    <div className="events">
+      <ul>
+        {events.map((event) => {
+          const { people: attendees } = event
+          return (
+            <div className="event">
+              <li key={event.id}>
+                <img src={event.eventImage} alt={event.name} />
+                <h5>
+                  {event.name} {event.eventType}
+                </h5>
+                <br />
+                <span>Organized by: {event.organizer} </span>
+                <br />
+                <Attendees 
+                attendees={attendees} 
+                updateEventAttendance={updateEventAttendance} 
+                event={event} 
+                />
+              </li>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  )
 }
