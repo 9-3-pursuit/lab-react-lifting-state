25f6be5552ced47fa59e46c6ad8022a81c66b79e
HaroldF415 <harold.f415@gmail.com>
2023-02-09 01:31:08 -0500

    Moved logic from Event=>Attendees=>Attendee

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Attendees from "./Attendees";
 
 const Event = ({ attendees, event, updateEventAttendance }) => {
   const [showAttendees, setShowAttendees] = useState(false);
@@ -17,38 +18,11 @@ const Event = ({ attendees, event, updateEventAttendance }) => {
         <br />
         <span>Organized by: {event.organizer} </span>
         <br />
-        <>
+        <div className="event">
           <button onClick={toggleEventAttendees}>{!showAttendees ? "Show Attendees" : "Hide Attendees"}</button>
 
-          {showAttendees ? (
-            <div className="attendees">
-              {attendees.map((attendee, index) => (
-                <>
-                  <div key={attendee.id} className="attendee">
-                    <p>
-                      <img src={attendee.avatar} alt={attendee.firstName} />
-                      {"   "}
-                      <span>
-                        {" "}
-                        {attendee.firstName} {attendee.lastName}{" "}
-                      </span>
-                    </p>
-                    <p>
-                      <button className="clickable" onClick={() => updateEventAttendance(event.id, attendee.id)}>
-                        Attending:
-                      </button>
-                      <span>{attendee.attendance ? "✅" : "❌"}</span>
-                    </p>
-
-                    <p>
-                      <span>Note:</span> {attendee.note}
-                    </p>
-                  </div>
-                </>
-              ))}
-            </div>
-          ) : null}
-        </>
+          {showAttendees ? <Attendees attendees={attendees} updateEventAttendance={updateEventAttendance} event={event} /> : null}
+        </div>
       </li>
     </>
   );
