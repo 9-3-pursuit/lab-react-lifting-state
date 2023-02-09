59ea902ed8ef42c4669f09ee1fb5f118c668a2e1
Keeanu Rodriguez <keeanu7@gmail.com>
2023-02-08 22:47:54 -0500

    the best I could do

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,31 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees";
+
+export default function Event({ events}, { setEvents }) {
+
+  console.log(events)
+  return (
+
+    <div className="events">
+      <ul>
+        {events.map((event) => {
+          const { people: attendees } = event;
+
+          return (
+            <>
+              <li className="event" key={event.id}>
+                <img src={event.eventImage} alt={event.name} />
+                <h5>
+                  {event.name} {event.eventType}
+                </h5>
+                <br />
+                <span>Organized by: {event.organizer} </span>
+                <br />
+                <Attendees attendees={attendees} events={events} setEvents={setEvents}/>
+                  </li>
+            </>
+          );
+        })}
+      </ul>
+    </div>
+  )
 }
