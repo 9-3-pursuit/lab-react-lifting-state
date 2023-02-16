524f240cc012a43d4e7a304e579923d6b8252eac
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-16 14:43:23 -0500

    Adds key to select section map return

--- src/Components/Form/SelectSection.js
+++ src/Components/Form/SelectSection.js
@@ -5,8 +5,10 @@ export default function SelectSection({ setSelectOption }) {
     <>
       <label htmlFor="eventType">Event type:</label>
       <select id="eventType" onChange={handleSelectChange}>
-        {selectOptions.map(({ value }) => (
-          <option value={value}>{value}</option>
+        {selectOptions.map(({ id, value }) => (
+          <option key={id} value={value}>
+            {value}
+          </option>
         ))}
       </select>
     </>
@@ -14,10 +16,10 @@ export default function SelectSection({ setSelectOption }) {
 }
 
 const selectOptions = [
-  { value: "" },
-  { value: "Birthday" },
-  { value: "Anniversary" },
-  { value: "Intramural Sport" },
-  { value: "Watch Party" },
-  { value: "Wedding" },
+  { id: 0, value: "" },
+  { id: 1, value: "Birthday" },
+  { id: 2, value: "Anniversary" },
+  { id: 3, value: "Intramural Sport" },
+  { id: 4, value: "Watch Party" },
+  { id: 5, value: "Wedding" },
 ];
