42c72f6a7a9004d8d0c1e920158052d6d7f8817f
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-15 21:29:47 -0500

    Moves handler functions into component

--- src/App.js
+++ src/App.js
@@ -8,24 +8,6 @@ import Footer from "./Components/Footer";
 function App() {
   const [events, setEvents] = useState(eventsData);
 
-  function handleAddEvent(event) {
-    setEvents([event, ...events]);
-  }
-
-  function updateEventAttendance(eventId, attendeeId) {
-    const eventArray = [...events];
-    const eventIndex = eventArray.findIndex((event) => eventId === event.id);
-    const event = { ...eventArray[eventIndex] };
-    const personIndex = event.people.findIndex(
-      (person) => person.id === attendeeId
-    );
-    const peopleArray = [...event.people];
-    peopleArray[personIndex].attendance = !peopleArray[personIndex].attendance;
-    event.people = peopleArray;
-    eventArray[eventIndex] = event;
-    setEvents(eventArray);
-  }
-
   return (
     <div className="App">
       <Header />
