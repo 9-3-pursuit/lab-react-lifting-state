006d6f20c87d16b2bd7459b15d12d3f36bef483b
YasserS-21 <saadi.yasser04@gmail.com>
2023-03-02 02:38:44 -0500

    Cursed last cypress test

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,30 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees"
+import { useState } from "react";
+
+export default function Event({setEvents, events}) {
+
+  return(<>
+  <div className="events">
+          <ul>
+            {events.map((event) => {
+              const { people: attendees } = event;
+              return (
+                <>
+                  <li key={event.id}>
+                    <img src={event.eventImage} alt={event.name} />
+                    <h5>
+                      {event.name} {event.eventType}
+                    </h5>
+                    <br />
+                    <span>Organized by: {event.organizer} </span>
+                    <br />
+                    <>
+                     <Attendees attendees={attendees} event = {event}events={events} setEvents={setEvents}/>
+                    </>
+                  </li>
+                </>
+              );
+            })}
+          </ul>
+        </div></>)
 }
