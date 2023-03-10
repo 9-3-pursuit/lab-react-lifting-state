c4c842a96b66f45005ede2f25e3ecee9caa28bbf
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-03-09 21:54:38 -0500

    modified app.js and new event form.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import eventsData from "./data";
 import { v1 as generateUniqueID } from "uuid";
-// import Attendees from "./Attendees";
-// import Event from "./Components/Event";
- import Footer from "./Components/Footer";
- import Header from "./Components/Header";
-// import NewEventForm from "./Components/NewEventForm";
+//import Attendees from "./Attendees";
+import Event from "./Components/Event";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import NewEventForm from "./Components/NewEventForm";
 
 function App() {
   const [events, setEvents] = useState(eventsData);
@@ -91,7 +91,7 @@ function App() {
   return (
     <div className="App">
       <>
-       < Header/>
+        <Header />
       </>
       <main>
         <div className="new-event">
@@ -205,7 +205,7 @@ function App() {
         </div>
       </main>
       <>
-       <Footer/> 
+        <Footer />
       </>
     </div>
   );
